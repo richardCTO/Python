54982b5aa0161e9a1bdb83e9a967eb71ad96d5e3
Richard <rmartin0120@gmail.com>
2019-11-22 20:40:53 -0500

    Just the begining of K Nearest Neighbors

--- Modeling/k_nearest_neighbors.py
+++ Modeling/k_nearest_neighbors.py
@@ -1,1 +1,9 @@
 #%%
+import numpy as np 
+from sklearn import preprocessing, neighbors
+from sklearn.model_selection import cross_val_score
+import pandas as pd
+
+df = pd.read_csv('Modeling/data/breast_cancer.csv')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
