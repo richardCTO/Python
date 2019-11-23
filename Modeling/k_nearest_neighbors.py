850048ba908fd48c9106731f89187b63b04aa207
Richard <rmartin0120@gmail.com>
2019-11-23 17:26:11 -0500

    Make comments great again!

--- Modeling/k_nearest_neighbors.py
+++ Modeling/k_nearest_neighbors.py
@@ -5,5 +5,15 @@ from sklearn.model_selection import cross_val_score
 import pandas as pd
 
 df = pd.read_csv('Modeling/data/breast_cancer.csv')
+
+# replaces all ? in data set with -99999 which
+# is done because most data models know that this 
+# value is an outlier and disregards it
 df.replace('?', -99999, inplace=True)
+
+# drop this colum because it is not needed 
+# in determianing breast cancer, not useful 
+# for the other data values
 df.drop(['id'], 1, inplace=True)
+
+# %%
