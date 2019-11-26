9b33d0ca9828eedbc60017cc0643762e135e6277
Richard <rmartin0120@gmail.com>
2019-11-26 12:29:13 -0500

    Finally! K Nearest Neighbors Classifier works!

--- Modeling/k_nearest_neighbors.py
+++ Modeling/k_nearest_neighbors.py
@@ -2,9 +2,25 @@
 import pandas as pd 
 
 df = pd.read_csv('Modeling/data/breast_cancer.csv')
+df.replace('?', -99999, inplace=True)
 df.head()
 # %%
 import numpy as np
 from sklearn.model_selection import train_test_split
+
 X = np.array(df.drop("class", axis = 1))
 y = np.array(df["class"])
+
+# 20% training and 80% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# %%
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+knn = KNeighborsClassifier(n_neighbors=4)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+print("Accuracy", metrics.accuracy_score(y_test, y_pred))
+
+# %%
