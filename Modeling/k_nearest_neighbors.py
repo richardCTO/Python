a3bdfe5b981d690945d8c06f4f002891c1aa150a
Richard <rmartin0120@gmail.com>
2019-11-27 15:13:27 -0500

    Add some comments

--- Modeling/k_nearest_neighbors.py
+++ Modeling/k_nearest_neighbors.py
@@ -8,6 +8,8 @@ df.head()
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# Remove the class column, as this could
+# affect the data training 
 X = np.array(df.drop("class", axis = 1))
 y = np.array(df["class"])
 
@@ -18,6 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
+# This is the classifier
 knn = KNeighborsClassifier(n_neighbors=4)
 knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
