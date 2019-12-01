b28fac1f1fc7614b6be5b5a24af5742c39c4cfae
Richard <rmartin0120@gmail.com>
2019-11-30 20:07:01 -0500

    KNN outline

--- Modeling/knn.py
+++ Modeling/knn.py
@@ -1,11 +1,18 @@
 #%%
+import numpy as np
 # K nearest neighbor
 class KNN:
     def __init__(self, k=3):
         self.k = k
         
     def fit(self, x, y):
-        pass
+        self.X_train = X 
+        self.y_train = y
+        
     
     def predict(self, x):
+        predicted_labels = [self._predict(x) for x in X]
+        return np.array(predicted_labels)
+        
+    def _predict(self, x):
         pass
