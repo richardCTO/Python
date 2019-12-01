6ced79a70eff4c9fd4b2e0598d0a5c28226d6d4c
Richard <rmartin0120@gmail.com>
2019-11-30 19:52:56 -0500

    A diffrent K nearest neighbor

---
+++ Modeling/knn.py
@@ -0,0 +1,11 @@
+#%%
+# K nearest neighbor
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+        
+    def fit(self, x, y):
+        pass
+    
+    def predict(self, x):
+        pass
