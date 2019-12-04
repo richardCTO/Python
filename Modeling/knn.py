50680f9afeda1c5462f158bf261995317a9a11da
Richard <rmartin0120@gmail.com>
2019-12-04 13:27:02 -0500

    Dataset columns

--- Modeling/knn.py
+++ Modeling/knn.py
@@ -11,8 +11,8 @@ class KNN:
     def __init__(self, k=3):
         self.k = k
         
-    def fit(self, x, y):
-        self.X_train = x 
+    def fit(self, X, y):
+        self.X_train = X 
         self.y_train = y
         
     def predict(self, x):
--- Modeling/pokemon.py
+++ Modeling/pokemon.py
@@ -1,7 +1,15 @@
 #%%
-import numpy as np 
-import pandas as pd
-
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 pokemon_data = pd.read_csv("Modeling/data/pokemon/pokemon.csv")
 pokemon_data.head(10)
+pokemon_data.tail()
+
+# %%
+# prints out all the columns for the data set
+pokemon_data.columns
+
+# %%
